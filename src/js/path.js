bd3653f81eff5f715d7463273fd0095a54ad67ac
Michael Browning <mrbrowning@uber.com>
2015-03-31 14:27:08 -0700

    track angle difference between mouseup and mousedown so that the nut doesn't skip when we drag again

--- src/js/path.js
+++ src/js/path.js
@@ -5,18 +5,33 @@ var NUT_RADIUS = 100,
     NUT_CENTER_RADIUS = NUT_RADIUS - 50,
     TRANSLATE_STRING = "translate(" + NUT_CENTER + ", " + NUT_CENTER + ")";
 
-function dragged() {
-  var angle = (Math.atan((d3.event.y - NUT_CENTER) / (d3.event.x - NUT_CENTER)) /
-               Math.PI * 180),
-      rotateCenter = NUT_CENTER;
+function angleTo(angleX, angleY, centerX, centerY) {
+  return Math.atan2(angleX - centerX, angleY - centerY);
+}
 
-  svg.selectAll("path")
-     .attr("transform",
-           "rotate(" + angle + "," + rotateCenter + "," + rotateCenter + ") " + TRANSLATE_STRING);
+function toDegrees(angle) {
+  return angle / Math.PI * 180;
+}
+
+function dragged() {
+  dragAngle = (
+    toDegrees(angleTo(d3.event.x, d3.event.y, NUT_CENTER, NUT_CENTER)) * -1 + adjustmentAngle
+  );
+
+  // Normalize the angle so that it remains in the range -180 <= x <= 180.
+  if (dragAngle > 180) {
+    dragAngle = dragAngle - 360;
+  } else if (dragAngle < -180) {
+    dragAngle = dragAngle + 360;
+  }
+
+  nutPath.attr(
+    "transform",
+    "rotate(" + dragAngle + "," + NUT_CENTER + "," + NUT_CENTER + ") " + TRANSLATE_STRING
+  );
 }
 
 function nut(data) {
-  // TODO: record mouseup location and adjust angle so we don't get jumps on next drag event.
   var centerRadius = data.centerRadius,
       arcRadius = centerRadius / 2,
       path = [],
@@ -41,12 +56,27 @@ function nut(data) {
   return path.join("");
 }
 
-var clickedX, clickedY;
+var adjustmentAngle = 0;
+var dragAngle = 0;
+var lastDraggedAngle = 0;
 var drag = d3.behavior.drag()
              .on("drag", dragged);
-var svg = d3.select("svg").call(drag);
+var svg = d3.select("svg")
+            .call(drag)
+            .on("mouseup", function() {
+              lastDraggedAngle = dragAngle;
+            })
+            .on("mousedown", function() {
+              // Account for the difference between where we last saw a mouseup and where we're
+              // starting a drag now -- otherwise the nut will jump from its last rotated-to angle.
+              adjustmentAngle = (
+                lastDraggedAngle -
+                toDegrees(angleTo(d3.event.x, d3.event.y, NUT_CENTER, NUT_CENTER)) *
+                -1
+              );
+            });
 
-svg.append("path")
-   .datum({radius: NUT_RADIUS, centerRadius: NUT_CENTER_RADIUS})
-   .attr("d", nut)
-   .attr("transform", TRANSLATE_STRING);
+var nutPath = svg.append("path")
+                 .datum({radius: NUT_RADIUS, centerRadius: NUT_CENTER_RADIUS})
+                 .attr("d", nut)
+                 .attr("transform", TRANSLATE_STRING);
